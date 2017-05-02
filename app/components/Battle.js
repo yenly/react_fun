71cac3088ed3c27b3fa2abddb243689afe9bd106
Yenly Ma <yencodes@gmail.com>
2017-05-02 11:57:31 -0700

    Add react-router-dom and create nav routes to each section of app.

---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div>
+        Battle Time!
+      </div>
+    )
+  }
+}
+
+module.exports = Battle;
