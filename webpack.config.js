c4042513ed6351797e1fc6cf116712806e7ec8a2
Yenly Ma <yencodes@gmail.com>
2017-05-17 08:57:07 -0700

    Deployed to firebase for hosting.

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@
 var path = require('path');
 // creates index.html in dist folder for us and include index_bundle.js
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
+// NODE_ENV to production
+// uglify - minify
 
-module.exports = {
+var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -24,3 +27,16 @@ module.exports = {
     template: 'app/index.html'
   })]
 }
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({ // set node environment inside code that  webpack would compile for us
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV) // strips out warnings
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin() // minify all our code
+  )
+}
+
+module.exports = config;
